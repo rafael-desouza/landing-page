2e58890d5086bd6f81d2a43e273c8a40d27801b7
rafael-desouza <rafaelferreiradesouza7@gmail.com>
2022-02-15 22:42:02 -0300

    feat: section about project and section tech

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,14 +1,60 @@
 import { gql } from 'graphql-request'
 
 const GET_LANDING_PAGE = gql`
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
-        alternativeText
+fragment logo on LandingPage {
+  logo {
+    url
+    alternativeText
+  }
+}
+
+fragment header on LandingPage {
+  header {
+    title
+    description
+    button {
+      label
+      url
+    }
+
+    image {
+      url
+      alternativeText
+    }
+  }
+}
+
+fragment sectionAboutProject on LandingPage {
+  sectionAboutProject {
+    title
+    description
+    image {
+      url
+      alternativeText
+    }
+  }
+}
+
+fragment sectionTech on LandingPage {
+  sectionTech {
+    title
+    techIcons {
+      title
+      icon {
         url
       }
     }
   }
+}
+
+query GET_LANDING_PAGE {
+  landingPage {
+    ...logo
+    ...header
+    ...sectionAboutProject
+    ...sectionTech
+  }
+}
 `
 
 export default GET_LANDING_PAGE
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,46 @@
+export type LandingPageProps = {
+  logo: LogoProps
+  header: HeaderProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+}
+
 export type LogoProps = {
   alternativeText: string
   url: string
 }
 
-export type LandingPageProps = {
-  logo: LogoProps
+export type HeaderProps = {
+  title: string
+  description: string
+  button: ButtonProps
+  image: ImageProps
+}
+
+export type ButtonProps = {
+  label: string
+  url: string
+}
+
+export type ImageProps = {
+  alternativeText: string
+  url: string
+}
+
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  image: ImageProps
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
 }
